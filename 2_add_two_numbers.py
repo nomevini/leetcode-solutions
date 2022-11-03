d89724812c795b93cc401dfc11360fc54da40d3e
Vinícius de Sousa Carvalho <sousav387@gmail.com>
2022-11-03 09:51:28 -0300

    problem 02 - add two numbers

--- 2_add_two_numbers.py
+++ 2_add_two_numbers.py
@@ -1,11 +1,27 @@
-# lista encadeada
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 def addTwoNumbers(l1, l2):
-    pass
-  
+    number01 = []
+    number02 = []
 
+    while l1:
+        number01.append(l1.val)
+        l1 = l1.next
+    while l2:
+        number02.append(l2.val)
+        l2 = l2.next
 
+    number01 = int(''.join([str(x) for x in number01[::-1]]))
+    number02 = int(''.join([str(x) for x in number02[::-1]]))
+    
+    list_result = list(str(number01 + number02))
+
+    # criar uma lista de list node sem fazer os nos
+    nodes = []
+    for number in list_result:
+        nodes.append(ListNode(val=int(number)))
+
+    # conectar os nos
+    for index, node in enumerate(nodes):
+        if index != 0:
+            node.next = nodes[index - 1]
+    
+    return nodes[-1]
